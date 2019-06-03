8801f3ccbe12465922c14152fa9c6fdf7a9c36e3
Koszmarny <pawelby2@gmail.com>
2019-06-03 12:37:19 +0200

    dodatkowe klasy

---
+++ src/DimensionMismatchException.java
@@ -0,0 +1,5 @@
+//public class DimensionMismatchException extends ArithmeticException {
+//    public boolean DimensionMismatchException(Vector[] vector1, Vector vector2){
+//
+//    }
+//}
--- src/EgzaminTest.java
+++
@@ -1,5 +0,0 @@
-public class EgzaminTest {
-    public static void main(String[] args) {
-        System.out.println(Egzamin.zaliczenie("ania bania   3 3.5 4 5 3 nb:4"));
-    }
-}
